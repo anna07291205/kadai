aa9149a3eb0547c63e06f1f1abb0c1be7f73eef3
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2024-01-30 20:35:37 +0900

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,2 +1,2 @@
-function gazo688()
-{document.getElementById('who688').innerHTML='<img src="./image/うらたぬき歌ってみた画像２.jpg" alt="オン">';}
+function gazo1()
+{document.getElementById('who1').innerHTML='<img src="./image/うらたぬき歌ってみた画像２.jpg" alt="オン">';}
