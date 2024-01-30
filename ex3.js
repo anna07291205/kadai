aa2df56fb082ea7ef06935cfc3ad84490aad8cde
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2024-01-30 21:15:23 +0900

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -1,2 +1,2 @@
 function gazo3()
-{document.getElementById('who3').innerHTML='<img src="./image/あほの坂田画像.jpg" alt="オン">';}
+{document.getElementById('who3').innerHTML='<img src="./image/あほの坂田実写.jpg" alt="オン">';}
