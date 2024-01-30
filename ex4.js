ae3091f3a22d0a8be730a8cf0b80255bd76f9eab
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2024-01-30 21:16:59 +0900

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -1,2 +1,2 @@
 function gazo4()
-{document.getElementById('who4').innerHTML='<img src="./image/センラ画像.jpg" alt="オン">';}
+{document.getElementById('who4').innerHTML='<img src="./image/センラ実写.jpg" alt="オン">';}
