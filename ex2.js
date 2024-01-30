1bc41c4debae969376fe64e7dc75c06fba12188b
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2024-01-30 20:31:20 +0900

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -1,2 +1,2 @@
 function gazo688()
-{document.getElementById('who688').innerHTML='<img src="./image/志麻.jpg" alt="オン">';}
+{document.getElementById('who688').innerHTML='<img src="./image/志麻画像.jpg" alt="オン">';}
